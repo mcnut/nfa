e5b695bc83dcf4d0ce78ae15cd916b21d51ce497
mcnut <G00328962@gmit.ie>
2018-04-08 12:21:14 +0100

    Initial commit. Project set-up

---
+++ shunt.go
@@ -0,0 +1,5 @@
+/*	Shunting Arm Algorithm
+Author: Adrian McNulty
+Based on instructional video "Shunting yard algorithm in Go"
+https://web.microsoftstream.com/video/9d83a3f3-bc4f-4bda-95cc-b21c8e67675e?channelId=f9970e30-b336-4145-8af3-a2bbe2938f5e&channelName=Graph%20Theory
+*/

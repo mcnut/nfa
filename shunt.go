8bda68cdd9aa47e8f8ff2201ad64a9f21a275d37
mcnut <G00328962@gmit.ie>
2018-04-08 12:29:25 +0100

    shunt.go file compiling

--- shunt.go
+++ shunt.go
@@ -3,3 +3,15 @@ Author: Adrian McNulty
 Based on instructional video "Shunting yard algorithm in Go"
 https://web.microsoftstream.com/video/9d83a3f3-bc4f-4bda-95cc-b21c8e67675e?channelId=f9970e30-b336-4145-8af3-a2bbe2938f5e&channelName=Graph%20Theory
 */
+
+package main
+
+// import fmt package to implement formatted I/O with functions analogous to C's printf and scanf.
+import (
+	"fmt"
+)
+
+func main() {
+	//	Test program is working
+	fmt.Println("Hello World!")
+}

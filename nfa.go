069d436c06c259c887ff47ded18fcc7bee8ed4de
mcnut <G00328962@gmit.ie>
2018-04-08 12:37:13 +0100

    nfa.go file compiling

--- nfa.go
+++ nfa.go
@@ -3,3 +3,14 @@ Author: Adrian McNulty
 Based on instructional video "go-thompson-final"
 https://web.microsoftstream.com/video/68a288f5-4688-4b3a-980e-1fcd5dd2a53b
 */
+
+package main
+
+import (
+	"fmt"
+)
+
+//	Test program is working
+func main() {
+	fmt.Println("Hello nfa")
+}
